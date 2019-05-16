d27be243e8f4d2ad226cba57f59cdb784e38e7d7
Brian Sekwakwa <briansenpai14@gmail.com>
2019-05-16 12:48:05 +0200

    added the footer and header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Header.css";
+import img from "./meme-face.png";
 
 function Header() {
   return (
     <div className="header">
-      <img src="" alt="" />
-      <h1>Meme Generator</h1>
+      <img src={img} width="100px" height="100px" alt="meme face" />
+      <h1>MEME GENERATOR</h1>
     </div>
   );
 }
