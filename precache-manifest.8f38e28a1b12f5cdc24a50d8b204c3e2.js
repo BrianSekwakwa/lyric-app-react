35df1f4141fcdf6e7396d9efc66c395d095c0346
Brian Sekwakwa <briansenpai14@gmail.com>
2019-05-22 15:24:30 +0200

    Updates

---
+++ precache-manifest.8f38e28a1b12f5cdc24a50d8b204c3e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "83cdb014ee20ad8acd12",
+    "url": "/recipe-finder/static/css/main.949e79e2.chunk.css"
+  },
+  {
+    "revision": "83cdb014ee20ad8acd12",
+    "url": "/recipe-finder/static/js/main.ccea4dbc.chunk.js"
+  },
+  {
+    "revision": "a36b70426969e096b3f9",
+    "url": "/recipe-finder/static/js/runtime~main.80ab8d2e.js"
+  },
+  {
+    "revision": "3575d8e9a208a3901e61",
+    "url": "/recipe-finder/static/css/2.ee43457e.chunk.css"
+  },
+  {
+    "revision": "3575d8e9a208a3901e61",
+    "url": "/recipe-finder/static/js/2.73b86d25.chunk.js"
+  },
+  {
+    "revision": "cccc592ec4591a4d8e34cde4ff77a096",
+    "url": "/recipe-finder/index.html"
+  }
+];
