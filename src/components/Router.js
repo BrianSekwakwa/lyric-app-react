d6f4929e6aa888c99a894bc850de89d122036f70
Brian Sekwakwa <briansenpai14@gmail.com>
2019-05-22 16:20:51 +0200

    removed browser router from react app and added hash router so i can use github pages

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import App from "../App";
 import OneRecipe from "./OneRecipe";
 
 function Router() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route path="/" component={App} exact />
         <Route path="/OneRecipe/:id" component={OneRecipe} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
